7144b7e17d484c96f4c074823f20c1efc0433fa0
nobare <vinicius.falconi@ahgora.com.br>
2024-02-20 18:56:18 -0300

    Initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import DatabaseRepositoryFactory from "./infra/factory/DatabaseRepositoryFactory";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
+import HttpController from "./infra/http/HttpController";
 import PgPromiseAdapter from "./infra/database/PgPromiseAdapter";
+import UsecaseFactory from "./infra/usecase/UsecaseFactory";
 
 // main
-const connection = new PgPromiseAdapter();
+const connection = new PgPromiseAdapter().connect();
+const repositoryFactory = new DatabaseRepositoryFactory(connection);
+const usecaseFactory = new UsecaseFactory(repositoryFactory);
 const httpServer = new ExpressAdapter();
-connection.connect();
+new HttpController(httpServer, usecaseFactory);
 httpServer.listen(3001);
